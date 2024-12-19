62b4f388da2e7f79d797155c2527b19cba4452ef
Dubrovskaya Oksana <oxanadu@yandex.ru>
2024-12-19 17:30:36 +0300

    112

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react'
 import Snowfall from 'react-snowfall'
 import './App.css'
+import s from './img/snow.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className={s.wrapper}>
       <Snowfall
         // Changes the snowflake color
-        color="blue"
+        color="#fff"
         // Applied to the canvas element
-        style={{ background: '#fff' }}
+        style={{  opacity:'0.5' }}
         // Controls the number of snowflakes that are created (default 150)
         snowflakeCount={200}
       />
-    </>
+    </div>
   )
 }
 
