154e33108865dc1dc38077af2aeb0fa73b407eaf
Dubrovskaya Oksana <oxanadu@yandex.ru>
2024-12-20 13:09:59 +0300

    113

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+
 import Snowfall from 'react-snowfall'
 import './App.css'
 import s from './img/snow.module.css'
+import music from './img/47835607.mp3'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+const audioRef = useRef(null)
 
+const handlePlay = () => {
+  audioRef.current.play();
+};
   return (
     <div className={s.wrapper}>
+      <audio
+       ref={audioRef}
+        
+        controls={true} >
+        <source type="audio/mp3" src={music} />
+      </audio>
       <Snowfall
         // Changes the snowflake color
         color="#fff"
         // Applied to the canvas element
-        style={{  opacity:'0.5' }}
+        style={{ opacity: '0.5' }}
         // Controls the number of snowflakes that are created (default 150)
         snowflakeCount={200}
       />
